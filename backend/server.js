cafe32a1718b9b5b1bbb5f25fffe2100403363a9
Ishant Awasthi <ishantawasthi350@gmail.com>
2026-05-02 21:47:36 +0530

    fix4

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ connectDB();
 // Security & parsing middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+  origin: ['http://localhost:5173', 'https://ethara-plum.vercel.app'],
+  credentials: true
+}))
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
